f6386a43e13889297dfd9684b68eb8891f49b72f
jihongPark <ggikko@hotmail.com>
2016-05-05 16:55:21 +0900

    just

--- RxTextApplication/app/src/main/java/ggikko/me/rxtextapplication/MainActivity.java
+++ RxTextApplication/app/src/main/java/ggikko/me/rxtextapplication/MainActivity.java
@@ -2,6 +2,11 @@ package ggikko.me.rxtextapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import rx.Observable;
+import rx.Observer;
+import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable<String> firstObservable
+                = Observable.just("Hello ggikko!-");
+
+        Observer<String> firstObserver = new Observer<String>() {
+            @Override
+            public void onCompleted() {
+            }
+
+            @Override
+            public void onError(Throwable e) {
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d("ggikko", s);
+            }
+        };
+
+        firstObservable.subscribe(firstObserver);
     }
+
 }
